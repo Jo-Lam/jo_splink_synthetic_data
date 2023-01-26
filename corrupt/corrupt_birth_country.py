e7ecd78882eaa53b18f8f7cdec7aee712783fac9
Joseph Lam <joseph.lam.18@ucl.ac.uk>
2023-01-26 14:19:22 +0000

    Delete corrupt_birth_country.py

--- corrupt/corrupt_birth_country.py
+++
@@ -1,5 +0,0 @@
-def birth_country_gen_uncorrupted_record(formatted_master_record, record_to_modify={}):
-
-    record_to_modify["birth_country"] = formatted_master_record["birth_countryLabel"]
-
-    return record_to_modify
